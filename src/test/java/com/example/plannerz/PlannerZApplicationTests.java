f01194321d39b195ed222575c5ad526c3982680f
DESKTOP-LRQTH57\ASUS <hjldd0523@naver.com>
2024-04-04 22:07:55 +0900

    Add plannerZ

---
+++ src/test/java/com/example/plannerz/PlannerZApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plannerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerZApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
